8b34f413fe9abfbd0c80d7603f928f2e9a3113bf
Samuel Carvajal <samuel@ics.ie>
2014-07-17 12:56:25 +0100

    A lot of plugins and things to use

--- app/routes.php
+++ app/routes.php
@@ -10,7 +10,25 @@
   | and give it the Closure to execute when that URI is requested.
   |
  */
+// Bind route parameters.
+Route::model('tournament', 'Tournament');
+Route::model('season', 'Season');
+Route::model('team', 'Team');
 
-Route::get('/', function() {
+
+
+Route::get('/',function() {
    return View::make('index');
 });
+
+
+Route::get('/tournaments', array('as' => 'tournaments', 'uses' => 'TournamentController@index'));
+
+Route::get('/tournaments/create', array('as' => 'create new tournament', 'uses' => 'TournamentController@create'));
+
+Route::get('/seasons', array('as' => 'seasons', 'uses' => 'SeasonController@index'));
+
+Route::get('/seasons/start', array('as' => 'start new season', 'uses' => 'SeasonController@create'));
+
+Route::get('/teams', array('as' => 'teams', 'uses' => 'TeamController@index'));
+
